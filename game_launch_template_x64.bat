4bbd5154bf93b03a975a7f2565a3587f0917111f
mitigd <mitigatingdiscord@proton.me>
2025-09-28 14:26:02 -0500

    add template bat files

---
+++ game_launch_template_x64.bat
@@ -0,0 +1,9 @@
+:: Set the environment variables using cmd syntax
+set VK_INSTANCE_LAYERS=VK_LAYER_AEJS_DeviceChooserLayer_x64
+set ENABLE_DEVICE_CHOOSER_LAYER=1
+set VULKAN_DEVICE_INDEX=0
+
+:: Launch the game
+*.exe
+
+:: ^^ change the * to the name of your game exe
---
+++ game_launch_template_x86.bat
@@ -0,0 +1,9 @@
+:: Set the environment variables using cmd syntax
+set VK_INSTANCE_LAYERS=VK_LAYER_AEJS_DeviceChooserLayer_x86
+set ENABLE_DEVICE_CHOOSER_LAYER=1
+set VULKAN_DEVICE_INDEX=0
+
+:: Launch the game
+*.exe
+
+:: ^^ change the * to the name of your game exe
